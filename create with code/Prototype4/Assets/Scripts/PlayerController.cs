a5741cc5564106525ce681a50d5fcf12c9dbaaef
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-01-12 09:13:39 -0800

    sdfg

--- create with code/Prototype4/Assets/Scripts/PlayerController.cs
+++ create with code/Prototype4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,22 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPwerup)
+        {
+            Debug.Log("Collided with:, " + collision.gameObject.name + " with powerup set to " + hasPowerup)
+        }
     }
 }
