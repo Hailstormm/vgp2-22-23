b0014bbe259398039cb5370bee23ad8a33e44dde
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-02-01 09:09:55 -0800

    finished 4.3 portrotype 4

--- create with code/Prototype4/Assets/Scripts/PlayerController.cs
+++ create with code/Prototype4/Assets/Scripts/PlayerController.cs
@@ -24,6 +24,8 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,6 +33,7 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
@@ -40,6 +43,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
