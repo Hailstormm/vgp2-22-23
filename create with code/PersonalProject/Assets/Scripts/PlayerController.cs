716872ceb32f53c766e79edd4b1dbdda07d408a9
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-02-07 09:49:31 -0800

    sdf

--- create with code/PersonalProject/Assets/Scripts/PlayerController.cs
+++ create with code/PersonalProject/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
+    private float zBound = 6;
    
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
     }
 }
