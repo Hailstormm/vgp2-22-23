8cffbf3e01fb5d31f22a3019fff10212e26d6b96
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-02-13 09:07:39 -0800

    v2

--- create with code/PersonalProject/Assets/Scripts/SpawnManager.cs
+++ create with code/PersonalProject/Assets/Scripts/SpawnManager.cs
@@ -9,12 +9,18 @@ public class SpawnManager : MonoBehaviour
 
     private float zEnemySpawn = 12.0f;
     private float xSpawnRange = 16.0f;
-    private float xPowerupRange = 5.0f;
+    private float zPowerupRange = 5.0f;
+    private float ySpawn = 0.75f;
+
+    private float powerupSpawnTime = 5.0f;
+    private float enemySpawnTime = 1.0f;
+    private float startDelay = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomEnemy", startDelay, enemySpawnTime);
+        InvokeRepeating("SpawnPowerup", startDelay, powerupSpawnTime);
     }
 
     // Update is called once per frame
@@ -22,4 +28,25 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
+    void SpawnRandomEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
+
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+    }
+
+    void SpawnPowerup()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, randomZ);
+
+        Instantiate(powerup, spawnPos, powerup.gameObject.transform.rotation);
+    }
+
 }
