4bd03a809020b98847fb79dc68782af2fd88a8da
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-03-14 09:10:21 -0700

    cvbn

--- create with code/Prototype5/Assets/Scripts/GameManager.cs
+++ create with code/Prototype5/Assets/Scripts/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -31,12 +31,13 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
         }
     }
 
-    void UpdateScore(int scoreToAdd)
+    public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "Score: "
+        scoreText.text = "Score: " + score;
     }
 }
--- create with code/Prototype5/Assets/Scripts/Target.cs
+++ create with code/Prototype5/Assets/Scripts/Target.cs
@@ -10,6 +10,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float ySpawnPos = -2;
     private float xRange = 4;
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -29,6 +31,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
