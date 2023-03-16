1e21416703ebbb115aeadb4ad1b06fef75754c26
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-03-16 09:10:30 -0700

    gameoveralmost

--- create with code/Prototype5/Assets/Scripts/GameManager.cs
+++ create with code/Prototype5/Assets/Scripts/GameManager.cs
@@ -9,13 +9,16 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     public TextMeshProUGUI scoreText;
     private int score;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
-        score = 0;
         UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -39,5 +42,11 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
