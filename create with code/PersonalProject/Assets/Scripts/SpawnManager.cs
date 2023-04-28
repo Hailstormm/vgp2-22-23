062156003a59ce7c52910a3c833282b9f404bd13
Hailstormm <113127527+Hailstormm@users.noreply.github.com>
2023-04-28 09:12:17 -0700

    sdfghj

--- create with code/PersonalProject/Assets/Scripts/SpawnManager.cs
+++ create with code/PersonalProject/Assets/Scripts/SpawnManager.cs
@@ -7,13 +7,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] enemies;
     public GameObject powerup;
 
-    private float zEnemySpawn = 12.0f;
+    private float zEnemySpawn = 15.0f;
     private float xSpawnRange = 16.0f;
     private float zPowerupRange = 5.0f;
     private float ySpawn = 0.75f;
 
     private float powerupSpawnTime = 5.0f;
-    private float enemySpawnTime = 1.0f;
+    private float enemySpawnTime = 3.0f;
     private float startDelay = 1.0f;
 
     // Start is called before the first frame update
